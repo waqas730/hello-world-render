be435e00433750cb709017bad184be8aa7ea30c4
waqas730 <115456814+waqas730@users.noreply.github.com>
2025-06-02 07:33:05 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
@@ -7,4 +8,6 @@ def hello():
     return "Hello, World from Render!"
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
